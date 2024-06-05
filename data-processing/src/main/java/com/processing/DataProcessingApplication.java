05c5daec2a31170a48bba20869c1a44fcb02ea77
chenhongsheng <13640155378@qq.com>
2024-06-05 09:41:47 +0800

    init

---
+++ data-processing/src/main/java/com/processing/DataProcessingApplication.java
@@ -0,0 +1,16 @@
+package com.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chen
+ * @createDate 2024/6/4 22:23
+ */
+
+@SpringBootApplication
+public class DataProcessingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessingApplication.class, args);
+    }
+}
